7b4179e67daf34acc07603789df945112f7364bf
Nicolas Klaue <klaue.nico@gmail.com>
2023-12-12 15:53:29 +0100

    root directory

--- RabbitMQ/CSVReader.py
+++ RabbitMQ/CSVReader.py
@@ -1,4 +1,14 @@
 import csv
+
+#region root directory
+import os
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+#endregion
+
 with open('cp-national-datafile-csv.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
     for row in spamreader:
