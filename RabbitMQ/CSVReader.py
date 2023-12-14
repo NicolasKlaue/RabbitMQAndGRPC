8f68cb3a8e1516e77a2fdc0967b639183a370864
Nicolas Klaue <klaue.nico@gmail.com>
2023-12-14 17:29:43 +0100

    Python send messages and confirmation

--- RabbitMQ/CSVReader.py
+++ RabbitMQ/CSVReader.py
@@ -1,15 +1,37 @@
 import csv
+import pika
+import json
 
 #region root directory
 import os
-
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 os.chdir(dname)
-
 #endregion
 
-with open('cp-national-datafile-csv.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in spamreader:
-        print(', '.join(row))
+file_path = 'cp-national-datafile-csv.csv'
+with open(file_path, newline='') as csvfile:
+    csv_content = csvfile.read()
+    
+
+#region pika connection
+try:
+     connection = pika.BlockingConnection(pika.ConnectionParameters('localhost','5672'))
+     channel = connection.channel()
+
+     queue_name = 'MyQueue'
+     channel.queue_declare(queue=queue_name)
+     channel.confirm_delivery()
+     message = {'filename':file_path,
+               'content':csv_content}
+     print(f"Sent CSV file {file_path}")
+except Exception as e:
+    print("Connection could not be established ERROR:\t" + str(e))
+
+try:
+    channel.basic_publish(exchange='', routing_key=queue_name, body=json.dumps(message),mandatory=True)
+    print('Message publish was confirmed')
+    connection.close()
+except Exception as e:
+    print('Message could not be confirmed ERROR:\t' + str(e))
+#endregion
