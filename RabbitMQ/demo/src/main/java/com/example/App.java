f42f8174a2425161037117e6c86aa1dc86d1e2c4
Nicolas Klaue <klaue.nico@gmail.com>
2023-12-14 18:05:31 +0100

    Received messages

--- RabbitMQ/demo/src/main/java/com/example/App.java
+++ RabbitMQ/demo/src/main/java/com/example/App.java
@@ -1,13 +1,20 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App 
+
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+     
+         public static void main(String[] args) {
+             SpringApplication.run(App.class, args);
+         }
+     
+         @RabbitListener(queues = "MyQueue")
+         public void receiveMessage(String message) {
+             System.out.println("Received message: " + message);
+         }
 }
