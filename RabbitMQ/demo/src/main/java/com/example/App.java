5a4bc4b05d8b46f20aba05ae3bc5869204be5aba
Nicolas Klaue <klaue.nico@gmail.com>
2023-12-14 18:26:59 +0100

    Better output formating

--- RabbitMQ/demo/src/main/java/com/example/App.java
+++ RabbitMQ/demo/src/main/java/com/example/App.java
@@ -1,20 +1,43 @@
 package com.example;
 
+import org.json.JSONObject;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
 @SpringBootApplication
-public class App 
+public class App
 
 {
+
      
-         public static void main(String[] args) {
-             SpringApplication.run(App.class, args);
-         }
-     
-         @RabbitListener(queues = "MyQueue")
-         public void receiveMessage(String message) {
-             System.out.println("Received message: " + message);
-         }
+
+     public static void main(String[] args) {
+          SpringApplication.run(App.class, args);
+     }
+
+     @RabbitListener(queues = "MyQueue")
+     public void receiveMessage(String message) {
+          try {
+               ObjectMapper objectMapper = new ObjectMapper();
+               JsonNode jsonNode = objectMapper.readTree(message);
+               
+                       // Extract filename and content fields
+        String filename = jsonNode.get("filename").asText();
+        String content = jsonNode.get("content").asText();
+
+        // Print filename and content separately
+        System.out.println("Received message:");
+        System.out.println("Filename: " + filename);
+        System.out.println("Content:\n" + content);
+               // Process the message here
+           } catch (Exception e) {
+               System.err.println("Error processing message: " + e.getMessage());
+           }
+
+          }
 }
