2fb712f6f3c6d438167b72fae3054a582037abcb
Laura Lasso <lauralassoporto@gmail.com>
2023-12-25 13:31:28 +0100

    Corretions and comments GRPC NodeJS

--- grpc/CSVReader.py
+++ grpc/CSVReader.py
@@ -47,25 +47,30 @@ public class JavaServer {
 
 
 # NodeJS Server
+# libraries
 const grpc = require('grpc');
 const protoLoader = require('@grpc/proto-loader');
 
+# proto definition
 const packageDefinition = protoLoader.loadSync('proto/node.proto');
 const nodeProto = grpc.loadPackageDefinition(packageDefinition).node;
 
+# new GRPC server
 const server = new grpc.Server();
 
 server.addService(nodeProto.Node.service, {
+    # manage data reception
     sendData: (call, callback) => {
-        // Process data here
+        # data process 
         console.log('Received data from Python:', call.request.data);
 
-        // Respond with acknowledgment
+        # confirmation message
         callback(null, { message: 'Node.js server received the data.' });
     },
 });
 
 
+# specific port and direction
 server.bind('127.0.0.1:8080', grpc.ServerCredentials.createInsecure());
 console.log('Node.js gRPC server running at http://127.0.0.1:8080');
 server.start();
