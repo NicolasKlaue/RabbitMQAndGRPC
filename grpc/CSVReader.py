dc776e7443d7235ba87fed9a07deec7d95404d3b
Laura Lasso <lauralassoporto@gmail.com>
2023-12-26 11:30:26 +0100

    Java Corrections

--- grpc/CSVReader.py
+++ grpc/CSVReader.py
@@ -17,32 +17,7 @@ with open('cp-national-datafile-csv.csv', newline='') as csvfile:
         print(', '.join(row))
 
 
-# Java Server 
 
-public class JavaServer {
-    public static void main(String[] args) throws Exception {
-        # GRPC server, port: 9090 configuration
-        Server server = ServerBuilder.forPort(9090)
-                .addService(new JavaService()) # add service
-                .build(); # build service
-
-        server.start(); 
-        server.awaitTermination(); # wait until server finishes its execution
-    }
-
-    static class JavaService extends JavaGrpc.JavaImplBase {
-        @Override 
-        public void sendData(JavaRequest request, StreamObserver<JavaResponse> responseObserver) {
-            System.out.println("Received data from Python: " + request.getData()); 
-            JavaResponse response = JavaResponse.newBuilder() # creates the response for the Python client
-                    .setMessage("Java server received the data.")
-                    .build(); 
-
-            responseObserver.onNext(response); # sends the response created before
-            responseObserver.onCompleted(); # complete operation advice
-        }
-    }
-}
 
 
 
--- grpc/demo/src/main/java/com/example/App.java
+++ grpc/demo/src/main/java/com/example/App.java
@@ -1,13 +1,26 @@
-package com.example;
+// Java Server 
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class JavaServer {
+    public static void main(String[] args) throws Exception {
+        // GRPC server, port: 9090 configuration
+        Server server = ServerBuilder.forPort(9090)
+                .addService(new JavaService()) // add service
+                .build(); // build service
+
+        server.start(); 
+        server.awaitTermination(); // wait until server finishes its execution
+    }
+
+    static class JavaService extends JavaGrpc.JavaImplBase {
+        @Override 
+        public void sendData(JavaRequest request, StreamObserver<JavaResponse> responseObserver) {
+            System.out.println("Received data from Python: " + request.getData()); 
+            JavaResponse response = JavaResponse.newBuilder() // creates the response for the Python client
+                    .setMessage("Java server received the data.")
+                    .build(); 
+
+            responseObserver.onNext(response); // sends the response created before
+            responseObserver.onCompleted(); // complete operation advice
+        }
     }
 }
