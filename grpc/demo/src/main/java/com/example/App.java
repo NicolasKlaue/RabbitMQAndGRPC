c2b40b72417eb1c542303f366c23baef6d99bbad
Nicolas Klaue <klaue.nico@gmail.com>
2023-12-28 21:53:08 +0100

    CommunicationClass

--- grpc/demo/src/main/java/com/example/App.java
+++ grpc/demo/src/main/java/com/example/App.java
@@ -3,20 +3,24 @@ package com.example;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
+import communication.Communication.JsonDataRequest;
+import communication.Communication.JsonDataResponse;
 
 public class App extends CommunicationServiceGrpc.CommunicationServiceImplBase {
-    @Override
-    public void sendJsonData(JsonDataRequest request, StreamObserver<JsonDataResponse> responseObserver) {
-        // Process the received JSON data
-        System.out.println("Received JSON data: " + request.getJsonData());
-
-        // Send a response
-        JsonDataResponse response = JsonDataResponse.newBuilder()
-                .setStatus("Data received successfully")
-                .build();
-        responseObserver.onNext(response);
-        responseObserver.onCompleted();
-    }
+     @Override
+     public void sendJsonData(JsonDataRequest request, StreamObserver<JsonDataResponse> responseObserver) {
+         // Process the received JSON data
+     
+         System.out.println("Received JSON data: " + request.getJsonData());
+     
+         // Send a response
+         JsonDataResponse response = JsonDataResponse.newBuilder()
+                 .setStatus("Data received successfully")
+                 .build();
+         responseObserver.onNext(response);
+         responseObserver.onCompleted();
+     }
+     
 
     public static void main(String[] args) throws Exception {
         Server server = ServerBuilder.forPort(9090)
